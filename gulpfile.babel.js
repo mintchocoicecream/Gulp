f28f08ad0b10d0d305dff808c00f7ecd5f53cbf1
mintchocoicecream <sxxhnjng@gmail.com>
2022-06-16 23:47:56 +0900

    #1 Pug Compilation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,24 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
+
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest: "build"
+    }
+}
+
+const pug = () => 
+    gulp
+        .src(routes.pug.src)
+        .pipe(gpug())
+        .pipe(gulp.dest(routes.pug.dest));
+
+const clean = () => del(["build"]); //del["확장자나 폴더 이름"];
+
+const prepare = gulp.dest([clean]);
+
+const assets = gulp.dest([pug]);
+
+export const dev = gulp.series([prepare, assets]);
