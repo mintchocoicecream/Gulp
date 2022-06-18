ac065b8fca837d9b6ac5e40a2bf90a68a142ee62
mintchocoicecream <sxxhnjng@gmail.com>
2022-06-18 16:54:45 +0900

    #2.1 Dev Server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
     pug: {
@@ -15,10 +16,14 @@ const pug = () =>
         .pipe(gpug())
         .pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]); //del["확장자나 폴더 이름"];
+const clean = () => del(["build"]); 
 
-const prepare = gulp.dest([clean]);
+const webserver = () => gulp.src("build").pipe(ws({livereload: true, open: true}));
 
-const assets = gulp.dest([pug]);
+const prepare = gulp.series([clean]);
 
-export const dev = gulp.series([prepare, assets]);
+const assets = gulp.series([pug]);
+
+const postDev = gulp.series([webserver]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
